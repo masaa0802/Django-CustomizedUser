3695b2fefc0e9eb4f1266e91f7d0aa406c08a875
Your Name <masaakigoto0802@gmail.com>
2022-01-16 16:34:39 +0900

    ~187

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,4 @@
+from tabnanny import verbose
 from django.db import models
 from django.contrib.auth.models import (
   BaseUserManager, AbstractBaseUser, PermissionsMixin
@@ -45,3 +46,32 @@ class User(AbstractBaseUser, PermissionsMixin):
 
   def __str__(self):
     return self.email
+
+
+class Students(models.Model):
+
+  name = models.CharField(max_length=20)
+  age = models.IntegerField()
+  score = models.IntegerField()
+  school = models.ForeignKey(
+    'Schools', on_delete=models.CASCADE
+  )
+
+  class Meta:
+    db_table = 'students'
+    verbose_name_plural = '生徒'
+    ordering = ('age',)
+
+  def __str__(self):
+    return self.name + ':' + str(self.age)
+
+
+class Schools(models.Model):
+  name = models.CharField(max_length=20, verbose_name = '学校名')
+
+  class Meta:
+    db_table = 'schools'
+    verbose_name_plural = '学校'
+  def __str__(self):
+      return self.name
+
